25ef983c721a11e95f7ed7e5831f8ada90d6c4f6
Sebastian <sebastianlakla@gmail.com>
2025-09-20 17:02:27 +0200

    Created all basic styles for project

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,14 @@
 </script>
 
 <template>
-    <p>hello</p>
+    <p>Hello</p>
 </template>
 
-<style scoped lang="scss">
-
+<style lang="scss">
+@use "@/assets/sass/reset" as *;
+@use "@/assets/sass/fonts" as *;
+@use "@/assets/sass/colors" as *;
+p{
+    font-family: "Public Sans", sans-serif;
+}
 </style>
