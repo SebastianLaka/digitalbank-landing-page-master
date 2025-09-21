fa2fad6f16870f7a15ea1c32e41c71d805ad35b0
Sebastian <sebastianlakla@gmail.com>
2025-09-21 14:47:17 +0200

    Add nav icons to toggle menu

--- src/components/main-site/site-nav/NavMainSite.vue
+++ src/components/main-site/site-nav/NavMainSite.vue
@@ -1,15 +1,32 @@
 <script setup>
-import NavLogo from './NavLogo.vue';
-import Logo from "@/components/icons/logo-dark.svg"
+import NavLogo from './NavLogo.vue'
+import NavToggleIcon from './NavToggleIcon.vue'
+import Logo from '@/components/icons/logo-dark.svg'
+import hamburgerIcon from '@/components/icons/icon-hamburger.svg'
+import closeMenuIcon from '@/components/icons/icon-close.svg'
 
 const logoAttrs = {
-    logo: Logo,
-    alt: 'Digitalbank logo'
+  logo: Logo,
+  alt: 'Digitalbank logo',
 }
-
+const navIcons = {
+    hamburgerIcon: hamburgerIcon,
+    closeIcon: closeMenuIcon,
+    alt: 'Navigation icons which let for interaction'
+}
 </script>
 <template>
-    <NavLogo :src="logoAttrs.logo" :alt="logoAttrs.alt"/>
+  <nav class="nav-site wrapper">
+    <NavLogo :src="logoAttrs.logo" :alt="logoAttrs.alt" />
+    <NavToggleIcon :src="navIcons.hamburgerIcon" :alt="navIcons.alt"/>
+  </nav>
 </template>
 <style scoped lang="scss">
+@media (min-width: 320px){
+    .nav-site{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+}
 </style>
---
+++ src/components/main-site/site-nav/NavToggleIcon.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    isToggle: Boolean,
+    src: String,
+    alt: String
+})
+
+</script>
+<template>
+    <img :src="props.src" :alt="props.alt" :isToggle="props.isToggle">
+</template>
+<style scoped lang="scss">
+</style>
