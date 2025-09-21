862263eddbdef4d287a79ea06dd960edb4ea9bf4
Sebastian <sebastianlakla@gmail.com>
2025-09-21 11:54:41 +0200

    Add wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <div class="check wrapper">
     <p>Hello</p>
+    <p>hello2</p>
+  </div>
 </template>
 
 <style lang="scss">
-@use "@/assets/sass/reset" as *;
-@use "@/assets/sass/fonts" as *;
-@use "@/assets/sass/colors" as *;
-p{
-    font-family: "Public Sans", sans-serif;
+@use '@/assets/sass/reset' as *;
+@use '@/assets/sass/fonts' as *;
+@use '@/assets/sass/colors' as *;
+@use '@/assets/sass/wrapper' as *;
+.check {
+    display: flex;
+    justify-content: space-between;
+  p {
+    font-family: 'Public Sans', sans-serif;
+  }
 }
 </style>
